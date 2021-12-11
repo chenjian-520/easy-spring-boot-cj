150494b0fc71b9658fe1f12f256319589ef6bc7b
‘chenjian’ <563732435@qq.com>
2021-12-11 22:30:12 +0800

    first commit

---
+++ src/main/java/com/liuqi/cj/easyspringboot/EasySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.liuqi.cj.easyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasySpringBootApplication.class, args);
+    }
+
+}
