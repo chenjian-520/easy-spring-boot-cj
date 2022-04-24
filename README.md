7586a8a548a5c21edeb793f0ec076af571ff5c3c
‘chenjian’ <563732435@qq.com>
2022-04-24 22:06:58 +0800

    spring.banner.location 自定义横幅：配置banner

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # easy-spring-boot-cj
+springboot start 
+官方文档：https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.spring-application
--- src/main/java/com/liuqi/cj/easyspringboot/EasySpringBootApplication.java
+++ src/main/java/com/liuqi/cj/easyspringboot/EasySpringBootApplication.java
@@ -2,13 +2,15 @@ package com.liuqi.cj.easyspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 public class EasySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EasySpringBootApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(EasySpringBootApplication.class);
+        // Banner.mode.off 关闭
+        // springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
     }
 
 }
